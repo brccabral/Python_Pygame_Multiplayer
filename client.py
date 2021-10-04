55f814a52c795944f9e94aa75d8dd69735c84bfa
Bruno Cabral <cabral.brc@gmail.com>
2021-10-04 08:43:46 -0700

    reset window

--- client.py
+++ client.py
@@ -8,3 +8,8 @@ win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
 
 clientNumber = 0
+
+def redrawWindow():
+    win.fill((255,255,255))
+    pygame.display.update()
+
