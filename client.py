689f4d1b520c151472681794f07422d47756fc93
Bruno Cabral <cabral.brc@gmail.com>
2021-10-04 11:24:05 -0700

    clear client to play Rock Paper Scisors

--- client.py
+++ client.py
@@ -1,39 +1,20 @@
 # pip install pygame
 import pygame
+from game import Game
 from network import Network
 from player import Player
 
-width = 500
-height = 500
+width = 700
+height = 700
 
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
 
-clientNumber = 0
-
-def redrawWindow(win:pygame.Surface, player:Player, player2:Player):
-    win.fill((255,255,255))
-    player.draw(win)
-    player2.draw(win)
-    pygame.display.update()
+def redrawWindow(win:pygame.Surface, game:Game, player:Player):
+    win.fill((128,128,128))
+    pass
 
 def main():
-    run = True
-    n = Network()
-    p = n.getP()
-
-    clock = pygame.time.Clock()
-
-    while run:
-        clock.tick(60)
-        p2 = n.send(p)
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                pygame.quit()
-                exit()
-        p.move()
-        redrawWindow(win, p, p2)
+    pass
 
 main()
