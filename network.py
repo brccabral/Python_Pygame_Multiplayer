87adcaca29f495617eb396a1460f869870128e8e
Bruno Cabral <cabral.brc@gmail.com>
2021-10-04 10:12:10 -0700

    send and receive pickle objects

--- network.py
+++ network.py
@@ -1,4 +1,5 @@
 import socket
+import pickle
 
 class Network:
     def __init__(self) -> None:
@@ -6,23 +7,23 @@ class Network:
         self.server = "192.168.0.23"
         self.port = 5555
         self.addr = (self.server, self.port)
-        self.pos = self.connect()
+        self.p = self.connect()
         # print(self.id)
     
-    def getPos(self):
-        return self.pos
+    def getP(self):
+        return self.p
     
     def connect(self):
         try:
             self.client.connect(self.addr)
-            return self.client.recv(2048).decode()
+            return pickle.loads(self.client.recv(2048))
         except Exception as e:
             print(e)
     
     def send(self, data):
         try:
-            self.client.send(str.encode(data))
-            return self.client.recv(2048).decode()
+            self.client.send(pickle.dumps(data))
+            return pickle.loads(self.client.recv(2048))
         except socket.error as e:
             print(e)
 
