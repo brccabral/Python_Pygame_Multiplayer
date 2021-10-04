9070b683feeed69bd862fbebe74c022eba91fa0c
Bruno Cabral <cabral.brc@gmail.com>
2021-10-04 10:53:43 -0700

    setup objects Ids

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import socket
 from _socket import socket as sk
 from _thread import *
 import sys
+from game import Game
 
 from player import Player
 
@@ -27,11 +28,23 @@ connected = set()
 games = {}
 idCount = 0
 
-def threaded_client(conn:sk, player):
+def threaded_client(conn:sk, player, gameId):
     pass
 
 while True:
     conn, addr = s.accept()
     print("Connected to: ", addr)
 
-    start_new_thread(threaded_client, (conn))
+    idCount += 1
+    p = 0
+    # with 10 players we need 5 games
+    gameId = (idCount - 1)//2
+    # if id is odd, it means we need a new Game
+    if idCount % 2 == 1:
+        games[gameId] = Game(gameId)
+        print("Creating a new game...")
+    else:
+        games[gameId].ready = True
+        p = 1
+
+    start_new_thread(threaded_client, (conn, p, gameId))
