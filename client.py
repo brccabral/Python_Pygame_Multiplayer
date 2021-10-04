68e62ed46bf1a9cf5cdd07e7dfb7e1979de0ed85
Bruno Cabral <cabral.brc@gmail.com>
2021-10-04 08:46:21 -0700

    Player init

--- client.py
+++ client.py
@@ -9,6 +9,15 @@ pygame.display.set_caption("Client")
 
 clientNumber = 0
 
+class Player():
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.rect = (x,y,width,height)
+
 def redrawWindow():
     win.fill((255,255,255))
     pygame.display.update()
