51c07daf33eae907d77a23fd59699fdb086922b5
Bruno Cabral <cabral.brc@gmail.com>
2021-10-04 10:47:42 -0700

    clear server to implement Rock Paper Scisors

--- server.py
+++ server.py
@@ -22,39 +22,16 @@ except socket.error as e:
 s.listen(2)
 print("Waiting for connection, Server Started!")
 
-players = [Player(0,0,50,50,(255,0,0)), Player(100,100,50,50,(0,255,0))]
+connected = set()
+# multiple games will be based on id
+games = {}
+idCount = 0
 
 def threaded_client(conn:sk, player):
-    conn.send(pickle.dumps(players[player]))
-    reply = ""
-    while True:
-        try:
-            # 2048 bytes to receive
-            data = pickle.loads(conn.recv(2048))
-            players[player] = data
+    pass
 
-            if not data:
-                print("Disconnected")
-                break
-            else:
-                if player == 1:
-                    reply = players[0]
-                else:
-                    reply = players[1]
-                
-                print("Received: ", data)
-                print("Sending: ", reply)
-            conn.sendall(pickle.dumps(reply))
-        except Exception as e:
-            print(e)
-            break
-    print("Lost connection")
-    conn.close()
-
-currentPlayer = 0
 while True:
     conn, addr = s.accept()
     print("Connected to: ", addr)
 
-    start_new_thread(threaded_client, (conn,currentPlayer))
-    currentPlayer += 1
+    start_new_thread(threaded_client, (conn))
