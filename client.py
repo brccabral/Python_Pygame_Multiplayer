465bb608c58ee06307ae9f98fbc840f70f0b1173
Bruno Cabral <cabral.brc@gmail.com>
2021-10-04 11:30:03 -0700

    init Button class

--- client.py
+++ client.py
@@ -7,8 +7,27 @@ from player import Player
 width = 700
 height = 700
 
+pygame.font.init()
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
+
+class Button:
+    def __init__(self, text, x, y, color):
+        self.text = text
+        self.x = x
+        self.y = y
+        self.color = color
+        self.width = 150
+        self.height = 100
+
+    def draw(self, win: pygame.Surface):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
+        font = pygame.font.SysFont("comicsans", 40)
+        text = font.render(self.text, 1, (255,255,255))
+        cx = self.x + round(self.width/2) - round(text.get_width()/2)
+        cy = self.y + round(self.height/2) - round(text.get_height()/2)
+        win.blit(text, (cx, cy))
+    
 
 def redrawWindow(win:pygame.Surface, game:Game, player:Player):
     win.fill((128,128,128))
