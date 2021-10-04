564f8ad49c6771691f6de7d0fd844427059a7c3f
Bruno Cabral <cabral.brc@gmail.com>
2021-10-04 11:48:32 -0700

    Button click

--- client.py
+++ client.py
@@ -28,6 +28,13 @@ class Button:
         cy = self.y + round(self.height/2) - round(text.get_height()/2)
         win.blit(text, (cx, cy))
     
+    def click(self, pos):
+        x1 = pos[0]
+        y1 = pos[1]
+        if self.x <= x1 <= (self.x + self.width) and self.y <= y1 <= (self.y + self.height):
+            return True
+        else:
+            return False
 
 def redrawWindow(win:pygame.Surface, game:Game, player:Player):
     win.fill((128,128,128))
