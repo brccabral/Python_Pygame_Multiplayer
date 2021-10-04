13b8f033b308af09554b970633c4f8abd44b4777
Bruno Cabral <cabral.brc@gmail.com>
2021-10-04 10:08:44 -0700

    create player file

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 # pip install pygame
 import pygame
 from network import Network
+from player import Player
 
 width = 500
 height = 500
@@ -9,34 +10,6 @@ win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
 
 clientNumber = 0
-
-class Player():
-    def __init__(self, x, y, width, height, color):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
-        self.rect = (x,y,width,height)
-        self.vel = 3
-    
-    def draw(self, win):
-        pygame.draw.rect(win, self.color, self.rect)
-    
-    def move(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            self.x -= self.vel
-        if keys[pygame.K_RIGHT]:
-            self.x += self.vel
-        if keys[pygame.K_UP]:
-            self.y -= self.vel
-        if keys[pygame.K_DOWN]:
-            self.y += self.vel
-        self.update()
-    
-    def update(self):
-        self.rect = (self.x,self.y,self.width,self.height)
 
 def read_pos(pos_str:str):
     split_str = pos_str.split(",")
