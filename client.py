d743e646221635fd9fc16177e5ac5b610d327129
Bruno Cabral <cabral.brc@gmail.com>
2021-10-04 21:13:39 -0700

    Waiting for Player position

--- client.py
+++ client.py
@@ -41,7 +41,7 @@ def redrawWindow(win:pygame.Surface, game:Game, playerIndex):
     if not(game.connected()):
         font = pygame.font.SysFont("comicsans", 80)
         text = font.render("Waiting for Player...", 1, (255,0,0), True)
-        win.blit(text, (width/2-text.get_width()/2, height-text.get_height()/2))
+        win.blit(text, (width/2-text.get_width()/2, height/2-text.get_height()/2))
     else:
         font = pygame.font.SysFont("comicsans", 60)
         text = font.render("Your move", 1, (0,255,255))
@@ -51,7 +51,7 @@ def redrawWindow(win:pygame.Surface, game:Game, playerIndex):
 
         move1 = game.get_player_move(0)
         move2 = game.get_player_move(1)
-        
+
         if game.bothWent():
             text1 = font.render(move1, 1, (0,0,0))
             text2 = font.render(move2, 1, (0,0,0))
