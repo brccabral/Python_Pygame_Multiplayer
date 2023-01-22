b45a7270f78b81dc1746fecb76a292e2276ccf33
Bruno Cabral <cabral.brc@gmail.com>
2023-01-22 00:14:31 -0800

    get_ip()

--- network.py
+++ network.py
@@ -2,10 +2,24 @@ import socket
 import pickle
 
 
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # doesn't even have to be reachable
+        s.connect(("10.254.254.254", 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = "127.0.0.1"
+    finally:
+        s.close()
+    return IP
+
+
 class Network:
     def __init__(self) -> None:
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "192.168.0.23"
+        self.server = get_ip()
         self.port = 5555
         self.addr = (self.server, self.port)
         self.p = self.connect()
